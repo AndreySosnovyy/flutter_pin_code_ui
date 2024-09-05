f14178972611a06133c724ae4637dfea9fc9c1eb
Andrey Sosnovyy <andrey.sosnovyy@umbrellait.com>
2024-09-05 11:03:48 +0300

    Add example

---
+++ example/lib/pin_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class PinView extends StatefulWidget {
+  const PinView({super.key});
+
+  @override
+  State<PinView> createState() => _PinViewState();
+}
+
+class _PinViewState extends State<PinView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
